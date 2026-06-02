5ea74abf9ceca61be8a496053b016bdfa8e760fc
ar7-coffe <heitorpadovani2012@gmail.com>
2026-06-02 14:06:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,7 @@
-body{
-    margin:0;
-    background:#0a1225;
-    color:white;
-    font-family:Arial,sans-serif;
-}
-
-.hero{
+.inicio{
     height:100vh;
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
 }
 
-.hero h1{
-    font-size:80px;
-}
-
-.produtos{
+.versoes{
     min-height:100vh;
-    text-align:center;
-    padding:50px;
-}
-
-.cards{
-    display:flex;
-    justify-content:center;
-    gap:40px;
-    flex-wrap:wrap;
-}
-
-.card{
-    background:#15213f;
-    padding:20px;
-    border-radius:15px;
-    width:250px;
-}
-
-.card img{
-    width:100%;
 }
