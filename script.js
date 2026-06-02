609d394d3723bac3aa21ea1ea78b0a5a95474d0c
ar7-coffe <heitorpadovani2012@gmail.com>
2026-06-02 13:54:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-function clicar() {
-    fetch("http://127.0.0.1:5000/api")
-        .then(res => res.json())
-        .then(data => {
-            alert(data.mensagem);
-        });
+body{
+    margin:0;
+    background:#0a1225;
+    color:white;
+    font-family:Arial,sans-serif;
+}
+
+.hero{
+    height:100vh;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+}
+
+.hero h1{
+    font-size:80px;
+}
+
+.produtos{
+    min-height:100vh;
+    text-align:center;
+    padding:50px;
+}
+
+.cards{
+    display:flex;
+    justify-content:center;
+    gap:40px;
+    flex-wrap:wrap;
+}
+
+.card{
+    background:#15213f;
+    padding:20px;
+    border-radius:15px;
+    width:250px;
+}
+
+.card img{
+    width:100%;
 }
